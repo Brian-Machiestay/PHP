08fb1dd8b7cb5209f4c5be160fb55573036e8110
nanaoseikwakye <kokwakye@payswitch.com.gh>
2024-07-10 15:04:57 +0000

    initialize webman project

---
+++ app/controller/IndexController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace app\controller;
+
+use support\Request;
+
+
+class IndexController
+{
+    public function index(Request $request)
+    {
+        static $readme;
+        
+        # echo Test::$table;
+        if (!$readme) {
+            $readme = file_get_contents(base_path('README.md'));
+        }
+        return $readme;
+    }
+
+    public function view(Request $request)
+    {
+        return view('index/view', ['name' => 'webman']);
+    }
+
+    public function json(Request $request)
+    {
+        return json(['code' => 0, 'msg' => 'ok']);
+    }
+
+}
